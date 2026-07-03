7920b415f264ad6190567d68d54a565972247ad0
bautista.gerry <bautistagerry2006@gmail.com>
2026-07-02 23:28:30 -0300

    trabajoo-practicoooo

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -18,4 +18,14 @@ authRouter.post(
     authController.login
 )
 
+authRouter.post(
+    '/forgot-password',
+    authController.forgotPassword
+)
+
+authRouter.post(
+    '/reset-password',
+    authController.resetPassword
+)
+
 export default authRouter
